1719238310d7308f44176216019c107385a0b381
Lakshyadeep Gogoi <Lakshyadeep19@gmail.com>
2022-10-06 23:55:39 +0530

    updated 4
    updated the CSRF Token for giving url looks good and add footer

--- textutiles/views.py
+++ textutiles/views.py
@@ -1,15 +1,4 @@
 # #I have created this file -Lakshya
-# from django.http import HttpResponse
-
-
-# code for video 6
-# def index(request):
-#     return  HttpResponse('''<h1>Navigation Bar----</h1>  <a href="https://lakshyadeepgogoi.netlify.app/"> My website <br> <a href="https://www.youtube.com/watch?v=AepgWsROO4k&ab_channel=CodeWithHarry/"> code with harry  ''')
-# def about(request):
-#     return  HttpResponse("About lakshya bhai")
-# # def contact(request):
-#     return  HttpResponse("contact lakshya bhai")
-
 
 from concurrent.futures.process import _ExecutorManagerThread
 from django.http import HttpResponse
@@ -18,48 +7,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-    # a='''
-    # <button><a href="/about">About</a></button>
-    # <button><a href="/gallery">Gallery</a></button>
-    # <button><a href="/services">Services</a></button>
-    # <button><a href="/contact">Contact</a></button>
 
-    # '''
-    # return HttpResponse(a)
-
-# def about(request):
-#     a='''
-#     <h1>About</h1>
-#     <button><a href="/">Back</a></button>
-
-#     '''
-#     return HttpResponse(a)
-
-# def gallery(request):
-#     a='''
-#     <h1>Gallery</h1>
-#     <button><a href="/">Back</a></button>
-
-
-#     '''
-#     return HttpResponse(a)
-
-# def services(request):
-#     a='''
-#     <h1>Services</h1>
-#     <button><a href="/">Back</a></button>
-#     '''
-#     return HttpResponse(a)
 
 def analyze(request):
-    data = request.GET.get('text', 'No text entered')
+    data = request.POST.get('text', 'No text entered')
+    
 
-    remPunc = request.GET.get('punc', 'of')
-    caps = request.GET.get('caps', 'of')
-    newLineRem = request.GET.get('newLineRem', 'of')
-    spaceRem = request.GET.get('spaceRem', 'of')
+    remPunc = request.POST.get('punc', 'of')
+    caps = request.POST.get('caps', 'of')
+    newLineRem = request.POST.get('newLineRem', 'of')
+    spaceRem = request.POST.get('spaceRem', 'of')
 
-    charcount = request.GET.get('charcount', 'of')
+    charcount = request.POST.get('charcount', 'of')
     strr = data
     purpose = ""
 
@@ -72,23 +31,23 @@ def analyze(request):
         params = {'purpose': 'remove Punctuations', 'answer': tempStr}
         strr = tempStr
         purpose += " | Remove Punctuations "
-        # return render(request, 'analyze.html', params)
+        
     if caps == 'on':
         print("2", strr)
         strr = strr.upper()
         params = {'purpose': 'Caps', 'answer': strr}
         purpose += "| Caps |"
-        # return render(request, 'analyze.html', params)
+       
 
     if newLineRem == 'on':
         tempStr = ""
         for i in strr:
-            if i != '\n':
-                tempStr += i
+            if  i != '\n' and i !='\r':
+                tempStr +=   i
         params = {'purpose': 'New Line remove', 'answer': tempStr}
         strr = tempStr
         purpose += "| remove new line "
-        # return render(request, 'analyze.html', params)
+       
 
     if spaceRem == 'on':
         tempStr = ""
@@ -104,4 +63,4 @@ def analyze(request):
     if remPunc == 'on' or caps == 'on' or newLineRem == 'on' or spaceRem == 'on':
         return render(request, 'analyze.html', params)
     else:
-        return HttpResponse('error hai bhai')
+        return HttpResponse('ERROR!!! ')
